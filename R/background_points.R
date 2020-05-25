b7247fb9df2ad41bf8455c675ad21dd670402c89
Doi90 <davidpw@student.unimelb.edu.au>
2020-05-25 14:20:44 +1000

    Update background point function

--- R/background_points.R
+++ R/background_points.R
@@ -5,8 +5,9 @@ background_points <- function(species,
                               region){
 
   ## Read in full target group background data
+  #TODO filepath and/or make it a package data object
 
-  all_background <- readsRDS("")
+  all_background <- readRDS("bg.rds")
 
   ## Filter background points
 
@@ -15,12 +16,20 @@ background_points <- function(species,
 
   #TODO Add region filter here
 
-  filter_bg <- filter_bg
+  filter_bg <- mask_species_data(filter_bg,
+                                 region)
+
+  ## Remove observations without dates
+
+  filter_bg <- filter_bg[!is.na(filter_bg$Date), ]
+
+  ## Remove old observations
+
+  filter_bg <- filter_bg[filter_bg$Date >= "1970-01-01", ]
 
 
   ## Sample from remaining background points
   ##TODO Set number of samples based on number of presences?
-  ##TODO Column subsetting
 
   if(nrow(filter_bg) >= 1000){
 
@@ -28,22 +37,40 @@ background_points <- function(species,
                            1000,
                            replace = FALSE), ]
 
+    bg$value <- 0
+
   } else {
 
     ## Generate background points
 
-    bg <- dismo::randomPoints(env_data[[1]],
-                              10000)
+    bg_dismo <- dismo::randomPoints(env_data[[1]],
+                                    10000)
 
-    bg$value <- 0
-
-    colnames(bg) <- c("lon",
-                      "lat",
-                      "value")
+    bg <- data.frame(ID = NA,
+                     Origin = NA,
+                     Species = NA,
+                     Longitude = bg_dismo[ , 1],
+                     Latitude = bg_dismo[ , 2],
+                     Date = NA,
+                     Basis.of.Record = NA,
+                     Locality = NA,
+                     Institute = NA,
+                     Collection = NA,
+                     Coordinate.Uncertainty.in.Metres =NA,
+                     Guild = NA,
+                     Value = 0,
+                     stringsAsFactors = FALSE)
 
   }
 
   ## Combine spp and bg dfs
 
+  spp_data$processed.data$Value <- 1
+
+  spp_data$processed.data <- rbind(spp_data$processed.data,
+                                   bg)
+
+  return(spp_data)
 
 }
+
