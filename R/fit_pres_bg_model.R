cbff75e6519c4b4d525dc8e21b0ca804d73f1d95
Doi90 <davidpw@student.unimelb.edu.au>
2020-06-04 18:23:40 +1000

    Format fit_pres_bg_data

--- R/fit_pres_bg_model.R
+++ R/fit_pres_bg_model.R
@@ -1,4 +1,3 @@
-
 #' Fit a Presence-Background Species Distribution Model
 #'
 #' @param spp_data
@@ -9,21 +8,36 @@
 #' @export
 #'
 #' @examples
-fit_pres_bg_model <- function(spp_data, tuneParam = TRUE, k = 5, parallel = TRUE, ncors = 4){
+
+fit_pres_bg_model <- function(spp_data,
+                              tuneParam = TRUE,
+                              k = 5,
+                              parallel = TRUE,
+                              ncors = 4){
 
   ## Estimate the tuned regularization parameter
+
   if(tuneParam){
-    k <- ifelse(sum(spp_data$value) <= k, sum(spp_data$value), k)
+
+    k <- ifelse(sum(spp_data$value) <= k,
+                sum(spp_data$value),
+                k)
+
     val <- which(names(spp_data) == "value")
+
     bestRegMult <- regularisedMaxent(data = spp_data[ , c(val, 5:ncol(spp_data))],
                                      kf = k,
                                      parallel = parallel,
                                      ncors = ncors)
-  } else{
+
+  } else {
+
     bestRegMult <- 1
+
   }
 
   ## Fit MaxEnt model
+
   best_mod <- maxnet::maxnet(p = spp_data$value,
                              data = spp_data[ , 5:ncol(spp_data)],
                              regmult = bestRegMult,
@@ -32,5 +46,6 @@ fit_pres_bg_model <- function(spp_data, tuneParam = TRUE, k = 5, parallel = TRUE
                                                     classes = "default"))
 
   return(best_mod)
+
 }
 
