48250594d727b8c29f8448033cf8f08b37f33578
Doi90 <davidpw@student.unimelb.edu.au>
2020-06-09 19:33:09 +1000

    Fix res specification to avoid duplicate system calls

--- R/utility_reproject_raster_gdal.R
+++ R/utility_reproject_raster_gdal.R
@@ -8,7 +8,7 @@ reproject_AA_gdal <- function(source_spatial_file,
                               output_file){
 
   #define property for output
-  res <- c(250,250)
+  res <- 250
   ext <- raster::extent(c(-1880371,2539811,-6124229,-1002843))
   proj <- sp::CRS("+proj=aea +lat_1=-18 +lat_2=-36 +lat_0=0 +lon_0=132 +x_0=0 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs")
 
