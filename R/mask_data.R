c44edb8bb350eab0fb27a3c6a9c39cde4aa30325
Roozbeh Valavi <valavi.r@gmail.com>
2020-06-30 12:00:49 +1000

    fix bias inversion

--- R/mask_data.R
+++ R/mask_data.R
@@ -14,49 +14,35 @@ mask_data <- function(env_data,
                       crop = FALSE){
 
   ## Don't forget the ACT
-
-  # if("NSW" %in% region){
-  #
-  #   region <- c(region, "ACT")
-  #
-  # }
-  #
-  # ## Convert region character
-  #
-  # region <- gsub("VIC", "Victoria", region)
-  # region <- gsub("NSW", "New South Wales", region)
-  # region <- gsub("QLD", "Queensland", region)
-  # region <- gsub("SA", "South Australia", region)
-  # region <- gsub("WA", "Western Australia", region)
-  # region <- gsub("NT", "Northern Territory", region)
-  # region <- gsub("TAS", "Tasmania", region)
-  # region <- gsub("ACT", "Australian Capital Territory", region)
-
-#
-#   ## Get state polygon data and make it sf
-#
-#   AUS.shapes <- rnaturalearth::ne_states("australia",
-#                                          returnclass = "sp")
-#
-#   AUS.shapes <- sp::spTransform(AUS.shapes,
-#                                 CRSobj = raster::crs(env_data))
-#
-#   AUS.shapes <- AUS.shapes[AUS.shapes$name %in% region, ]
-
   ## Add ACT to NSW
   if("NSW" %in% region){
     if(!"ACT" %in% region)
       region <- c(region, "ACT")
   }
 
-  ## read the borders and filter them
-  aus <- rnaturalearth::ne_states("australia",
-                                  returnclass = "sf")
-  aus <- aus[-c(6,9,10), ]
-  aus$states <- substr(aus$iso_3166_2, 4, nchar(aus$iso_3166_2))
-  aus <- sf::st_transform(aus, crs = raster::crs(env_data))
-  AUS.shapes <- aus[aus$states %in% region, ]
+  ## Convert region character
 
+  region <- gsub("VIC", "Victoria", region)
+  region <- gsub("NSW", "New South Wales", region)
+  region <- gsub("QLD", "Queensland", region)
+  region <- gsub("SA", "South Australia", region)
+  region <- gsub("WA", "Western Australia", region)
+  region <- gsub("NT", "Northern Territory", region)
+  region <- gsub("TAS", "Tasmania", region)
+  region <- gsub("ACT", "Australian Capital Territory", region)
+
+
+# Get state polygon data and make it sf
+
+  AUS.shapes <- rnaturalearth::ne_states("australia",
+                                         returnclass = "sf")
+
+  AUS.shapes <- sf::st_transform(AUS.shapes,
+                                 CRSobj = raster::crs(env_data))
+
+  AUS.shapes <- AUS.shapes[AUS.shapes$name %in% region, ]
+
+  ## crop the raster
   if(crop){
     env_data <- raster::crop(env_data, AUS.shapes)
   }
