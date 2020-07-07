2091338a778252d84dabad7ea08d69edb151291d
Roozbeh Valavi <valavi.r@gmail.com>
2020-07-07 22:32:51 +1000

    map_sp_data extra arguments

--- R/map_sp_data.R
+++ R/map_sp_data.R
@@ -1,12 +1,18 @@
 #' Map species data
 #'
 #' @param spp_data species data
+#' @param crs coordinate system
+#' @param zcol column name to show in colour; set NULL if not needed.
+#' @param only_presences Show only presence points
 #'
 #' @return
 #' @export
 #'
 #' @examples
-map_sp_data <- function(spp_data, crs = 3577){
-  occurrence <- sf::st_as_sf(spp_data$data, coords = c("Longitude", "Latitude"), crs = crs)
-  mapview::mapview(occurrence)
+map_sp_data <- function(spp_data, crs = 3577, zcol = "Value", only_presences = FALSE){
+  dt <- spp_data$data
+  if(only_presences)
+    dt <- dplyr::filter(dt, Value == 1)
+  occurrence <- sf::st_as_sf(dt, coords = c("Longitude", "Latitude"), crs = crs)
+  mapview::mapview(occurrence, zcol = zcol)
 }
